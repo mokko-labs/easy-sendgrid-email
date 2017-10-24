5daf2675f493acb89f48369bd327f64ae3fab016
Pritish Vaidya <pritishvaidya94@gmail.com>
2017-10-24 17:43:07 +0530

    Adding the test case for the reciever of the email and modifying test reporter

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var mocha = require('gulp-mocha');
 gulp.task('test', function() {
 gulp.src('./tests/*.js', {read: false})
   .pipe(mocha({
-    reporter: 'progress'
+    reporter: 'spec'
   }))});
 
 // Default task should always be run before committing.
--- tests/easyEmail.spec.js
+++ tests/easyEmail.spec.js
@@ -2,13 +2,8 @@ var config = require('../config');
 var easyEmail = require('../easy-sendgrid-email')(config.sendGrid.apiKey);
 
 var chai = require('chai')
-var chaiAsPromised = require("chai-as-promised");
 
 var expect = chai.expect
-var should = chai.should()
-
-chai.use(chaiAsPromised);
-
 
 var options;
 describe('should validate the sender', function () {
@@ -19,72 +14,92 @@ describe('should validate the sender', function () {
       message: 'demo content'
     }
   });
-  it('check if the sender is null', function () {
+  it('check if the receiver is null', function () {
     // Should always throw an error
     return easyEmail.send(options)
       .then(function(success) {
-        expect(success.statusCode).to.deep.equal(400)
+        return expect(success.statusCode).to.deep.equal(400)
       })
       .catch(function(error) {
-        expect(error.response.statusCode).to.deep.equal(400)
+        return expect(error.response.statusCode).to.deep.equal(400)
       })
   });
-  it('check if the sender is invalid email', function () {
+  it('check if the receiver is invalid email', function () {
     options.to = 'pritish'
     // Should always throw an error
     return easyEmail.send(options)
       .then(function(success) {
-        expect(success.statusCode).to.deep.equal(400)
+        return expect(success.statusCode).to.deep.equal(400)
       })
       .catch(function(error) {
-        expect(error.response.statusCode).to.deep.equal(400)
+        return expect(error.response.statusCode).to.deep.equal(400)
       })
   });
-  it('check if the sender is valid email', function () {
+  it('check if the receiver is valid email', function () {
     options.to = 'pritish@mokko.io'
     // Should always succeed
     return easyEmail.send(options)
       .then(function(success) {
-        expect(success.statusCode).to.deep.equal(202)
+        return expect(success.statusCode).to.deep.equal(202)
       })
       .catch(function(error) {
-        expect(error.response.statusCode).to.deep.equal(202)
+        return expect(error.response.statusCode).to.deep.equal(202)
       })
   });
 });
 
-/*describe('should validate the sender', function () {
+describe('should validate the sender email and name', function () {
   before(function () {
-    var options = {
-      to
+    options = {
+      to: 'pritishvaidya94@gmail.com',
+      subject: 'demo subject',
+      message: 'demo content'
     }
   });
-  it('should result in three using the return style approach', function () {
-    var result = calculator.add(1, 2);
-    expect(result).equal(3);
+  it('check if the sender is null', function () {
+    // Should always throw an error
+    return easyEmail.send(options)
+      .then(function(success) {
+        return expect(success.statusCode).to.deep.equal(400)
+      })
+      .catch(function(error) {
+        return expect(error.response.statusCode).to.deep.equal(400)
+      })
+  });
+  it('check if the sender is invalid email', function () {
+    options.fromEmail = 'pritish'
+    // Should always throw an error
+    return easyEmail.send(options)
+      .then(function(success) {
+        return expect(success.statusCode).to.deep.equal(400)
+      })
+      .catch(function(error) {
+        return expect(error.response.statusCode).to.deep.equal(400)
+      })
+  });
+  it('check if the sender is valid email', function () {
+    options.fromEmail = 'pritish@mokko.io'
+    // Should always succeed
+    return easyEmail.send(options)
+      .then(function(success) {
+        return expect(success.statusCode).to.deep.equal(202)
+      })
+      .catch(function(error) {
+        return expect(error.response.statusCode).to.deep.equal(202)
+      })
+  });
+
+
+  it('check if the email name is null', function () {
+    options.fromName = null
+    options.fromEmail = 'pritish@mokko.io'
+    // Should always succeed
+    return easyEmail.send(options)
+      .then(function(success) {
+        return expect(success.statusCode).to.deep.equal(202)
+      })
+      .catch(function(error) {
+        return expect(error.response.statusCode).to.deep.equal(202)
+      })
   });
 });
-
-describe('should validate the sender', function () {
-  before(function () {
-    var options = {
-      to
-    }
-  });
-  it('should result in three using the return style approach', function () {
-    var result = calculator.add(1, 2);
-    expect(result).equal(3);
-  });
-});
-
-describe('should validate the sender', function () {
-  before(function () {
-    var options = {
-      to
-    }
-  });
-  it('should result in three using the return style approach', function () {
-    var result = calculator.add(1, 2);
-    expect(result).equal(3);
-  });
-});*/
