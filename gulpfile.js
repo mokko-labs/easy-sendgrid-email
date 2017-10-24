8f7ee174034f1577444624db106339087151a2db
Pritish Vaidya <pritishvaidya94@gmail.com>
2017-10-24 15:22:06 +0530

    Adding the gulp file and the required dependencies for testing

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+// Run all the test code.
+gulp.task('test', function() {
+gulp.src('./tests/*.js', {read: false})
+  .pipe(mocha({
+    reporter: 'progress'
+  }))});
+
+// Default task should always be run before committing.
+gulp.task('default', ['test']);
